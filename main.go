39548d2f4a095d90e945aae7ab25782229e3dbdf
Josh Bowen <jskybowen@gmail.com>
2022-07-14 18:04:52 -0600

    add badger db

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"math/big"
 
+	badger "github.com/dgraph-io/badger/v3"
+
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -14,7 +16,7 @@ import (
 )
 
 type rpcBlock struct {
-	Hash    common.Hash `json:"hash"`
+	TmHash  common.Hash `json:"hash"`
 	EthHash common.Hash `json:"eth_hash"`
 }
 
@@ -46,7 +48,7 @@ func getBlockHashesByNum(client *rpc.Client, args ...interface{}) (*rpcBlock, er
 }
 
 // walk the chain from height to head
-func walkChain(rawClient rpc.Client, client ethclient.Client, height uint64) {
+func walkChain(rawClient rpc.Client, client ethclient.Client, height uint64, db *badger.DB) {
 	head, err := client.BlockNumber(context.Background())
 	if err != nil {
 		panic(err)
@@ -56,12 +58,37 @@ func walkChain(rawClient rpc.Client, client ethclient.Client, height uint64) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(b)
-	}
+		// Add to the DB
+		txn := db.NewTransaction(true)
+		defer txn.Discard()
 
+		// tm to eth
+		err = txn.Set(b.TmHash.Bytes(), b.EthHash.Bytes())
+		if err != nil {
+			panic(err)
+		}
+		// eth to tm
+		err = txn.Set(b.EthHash.Bytes(), b.TmHash.Bytes())
+		if err != nil {
+			panic(err)
+		}
+		// Commit the transaction and check for error.
+		if err := txn.Commit(); err != nil {
+			panic(err)
+		}
+		fmt.Printf("height: %d\ttmHash: %v\tethHash: %v\n", i, b.TmHash, b.EthHash)
+	}
 }
 
 func main() {
+	// Open the Badger database located in the /tmp/badger directory.
+	// It will be created if it doesn't exist.
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	client, err := ethclient.Dial("http://ethermint0:8545")
 	if err != nil {
 		panic(err)
@@ -71,14 +98,14 @@ func main() {
 		panic(err)
 	}
 
-	walkChain(*rawClient, *client, 0)
-
 	// Walk the Ethermint chain starting from block 0
 	// Retrieve each block and parse out the "result.hash" and "result.eth_hash"
+	walkChain(*rawClient, *client, 0, db)
+
 	// Create a two kv pairs for each block, one from eth->tm and one from tm->eth
 	// Increment a block height counter
-	// start server
 
+	// start server
 	// proxy := goproxy.NewProxyHttpServer()
 	// proxy.Verbose = true
 	// log.Fatal(http.ListenAndServe(":8080", proxy))
