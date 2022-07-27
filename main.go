15f893cdf7e782dd3cad57a4bb6ebb6cc8ac47ba
Josh Bowen <jskybowen@gmail.com>
2022-07-26 22:45:34 -0600

    block

--- main.go
+++ main.go
@@ -89,19 +89,19 @@ func dbHashLookup(db *badger.DB, hash common.Hash) ([]byte, error) {
 	return item.ValueCopy(nil)
 }
 
-func (s *EthService) GetBlockByHash(hash string, full bool) (types.Header, error) {
+func (s *EthService) GetBlockByHash(hash string, full bool) (types.Block, error) {
 	ctx := context.Background()
 	dbHash, err := dbHashLookup(s.db, common.HexToHash(hash))
 	if errors.Is(err, badger.ErrKeyNotFound) {
 	} else if err != nil {
-		return types.Header{}, err
+		return types.Block{}, err
 	}
-	header, err := s.ethClient.HeaderByHash(ctx, common.BytesToHash(dbHash))
+	block, err := s.ethClient.BlockByHash(ctx, common.BytesToHash(dbHash))
 	if err != nil {
-		return types.Header{}, err
+		return types.Block{}, err
 	}
-	fmt.Println("Hash: ", header.Hash())
-	return *header, nil
+	fmt.Println("Hash: ", block.Hash())
+	return *block, nil
 }
 
 func server(errChan chan error, db *badger.DB, ethClient *ethclient.Client) {
